dd21d8063ed78d73e67553400dd0fbc4b741a0be
ahmedachora10 <gjr8195@gmail.com>
2022-10-01 23:15:31 +0100

    add commands route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Web\PostController;
 use App\Http\Controllers\Web\SubscriberController;
 use App\Models\Role;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +19,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     // auth()->user()->attachRole('admin');
-//     return view('welcome');
-// })->name('home');
+Route::middleware('role:admin')->group(function ()
+{
+    Route::get('clear-cache', function () {
+
+        Artisan::call('cache:clear');
+        Artisan::call('view:clear');
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
+
+        return 'done';
+
+    })->name('commands.cache');
+
+    Route::get('storage-link', function () {
+
+        Artisan::call('storage:link');
+
+        return 'done';
+
+    })->name('commands.link');
+
+});
+
 
 Route::controller(HomeController::class)->group(function ()
 {
