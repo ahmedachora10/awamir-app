1a136c21e15f724d76f7d450ab34143f74f158eb
ahmedachora10 <gjr8195@gmail.com>
2022-10-19 16:21:14 +0100

    update whatsapp share button

--- resources/views/pages/web/jobs/show.blade.php
+++ resources/views/pages/web/jobs/show.blade.php
@@ -83,7 +83,7 @@
 
                         @if (!empty($job->whatsapp))
                             <a
-                                href="https://web.whatsapp.com/send?text=*{{ $job->name }}* %20%0A رابط التقديم: {{ route('web.jobs.show', $job) }} : %20%0A من قروب أوامر توظيف : {{ settings('telegram') }}">
+                                href="whatsapp://send?text=*{{ $job->name }}* %20%0A رابط التقديم: {{ route('web.jobs.show', $job) }} : %20%0A من قروب أوامر توظيف : {{ settings('telegram') }}">
                                 <button class="btn_job" style="background-color:{{ settings('whatsapp_share_bg') }} ">
                                     <i class="bi bi-whatsapp"></i> مشاركة عبر واتساب
                                 </button>
