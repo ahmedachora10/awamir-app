071dfa4a74ae79f2bae42405f060086e4ab1f6ac
ahmedachora10 <gjr8195@gmail.com>
2022-10-20 08:05:52 +0100

    decrease database query requests

--- app/Http/Livewire/Admin/JobsContainer.php
+++ app/Http/Livewire/Admin/JobsContainer.php
@@ -23,18 +23,11 @@ class JobsContainer extends Component
     public function mount()
     {
         $this->isAdmin = auth()->user()->hasRole('admin');
-        $this->posts = Post::with('city','country', 'category')->latest()->paginate(self::PAGINATE_NUMBER);
     }
 
     public function filter()
     {
-
-        // $data = [];
-        // if($this->search != '' ||  !is_null($this->search)) {
-        // }
-
         $this->search;
-
     }
 
     public function updatingSearch()
@@ -44,7 +37,6 @@ class JobsContainer extends Component
 
     public function render()
     {
-        // $jobs = $this->posts ?? [];
         return view('livewire.admin.jobs-container', [
             'jobs' => Post::with('city','country', 'category')->where('posts.name', 'like', "%".$this->search."%")->latest()->paginate(self::PAGINATE_NUMBER)
         ]);
